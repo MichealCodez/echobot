2ac55184fc405d18777cd7bd7376e901da31f5c8
MichealDavid1 <madtechnow@gmail.com>
2024-01-13 16:58:21 +0100

    setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main>
+      <Nav />
+      <section className="padding">
+        <Hero />
+      </section>
+      <section className="padding">
+        <StayConnected />
+      </section>
+      <section className="padding">
+        <PowerOfEchobot />
+      </section>
+      <section className="padding">
+        <TryIt />
+      </section>
+      <section className="padding">
+        <CustomerRevier />
+      </section>
+      <section className="padding">
+        <Hero />
+      </section>
+      <section className="padding">
+        <GetMobile />
+      </section>
+      <section className="padding">
+        <FAQ />
+      </section>
+      <section className="padding">
+        <Footer />
+      </section>
+    </main>
+  );
 }
