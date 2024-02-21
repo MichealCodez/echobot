fc2605e5ab8647fd4ed57ca885cc6989e1e92385
MichealDavid1 <madtechnow@gmail.com>
2024-02-21 18:55:24 +0100

    setting up customer review section

--- src/sections/CustomerRevier.jsx
+++ src/sections/CustomerRevier.jsx
@@ -4,9 +4,9 @@ import { customerReviews } from "../constants";
 const CustomerRevier = () => {
   return (
     <div>
-      <div>
+      <div className="max-w-[535px] text-center mb-5">
         <h3 className="h3">What clients have to say</h3>
-        <p className="font-normal leading-[150%] text-[#333D48]">
+        <p className="font-normal leading-[150%] text-[#333D48] my-4 px-12">
           Join the ranks of satisfied clients who have experienced the impact of
           EchoBot on their business
         </p>
@@ -14,10 +14,10 @@ const CustomerRevier = () => {
       <div className="relative max-w-[706px]">
         {customerReviews.map((value) => (
           <article key={value.name}>
-            <p>{value.review}</p>
+            <p className="max-w-[548px] text-center px-10">{value.review}</p>
             <figure>
               <figcaption>
-                <h3>{value.name}</h3>
+                <h3 className="h3">{value.name}</h3>
                 <p> {value.occupation}</p>
               </figcaption>
               <div className="absolute">
